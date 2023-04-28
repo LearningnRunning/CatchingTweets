9f66af8eaa0bc1e267c3ac7618d6a5e1b0572db1
Rocki_Kim <rocki.k1208@gmail.com>
2023-04-28 17:55:11 +0900

    FEAT : Streamlit ver

--- twitterStreamlit.py
+++ twitterStreamlit.py
@@ -9,6 +9,13 @@ from glob import glob
 import streamlit as st
 from streamlit_option_menu import option_menu
 
+################################################################
+path = "./data"
+keyWordList = [folder for folder in os.listdir(path) if os.path.isdir(os.path.join(path, folder))]
+
+################################################################
+
+
 def twitterAPI(query, max_results, day_num):
     client = tweepy.Client(bearer_token=BEARER_TOKEN, 
                         consumer_key=consumer_key, 
@@ -34,32 +41,43 @@ def twitterAPI(query, max_results, day_num):
     # Extract "data" value from dictionary
     tweets_data = tweets_dict['data'] 
     df = pd.json_normalize(tweets_data) 
-    print(df)
-    if df.bool():
-        df.to_csv('./data/twitter_{0}_{1}.csv'.format(query,datetime.now().strftime('%Y_%m_%d_%H_%M')), index=False, encoding='utf-8-sig')
+    # print(df)
+    # if df.bool():
+    if not os.path.exists(f"./data/{query}"):
+        os.mkdir(f"./data/{query}")
+    df.to_csv('./data/{0}/twitter_{0}_{1}.csv'.format(query,datetime.now().strftime('%Y_%m_%d_%H_%M')), index=False, encoding='utf-8-sig')
     return tweets_data
 
 with st.sidebar:
     name = option_menu("Twitter API", ["New Keyword", "Old Keyword"],
                        menu_icon="app-indicator")
 
-st.sidebar.header("정밀한 얼굴 분석으로 이상적인 메이크업 가이드를 찾아보세요.")
+st.sidebar.header("키워드로 찾아보세요.")
 
 if name == "New Keyword":
     st.title("TEST")
-    st.write("### 정밀한 얼굴 분석으로 이상적인 메이크업 가이드를 찾아보세요.")
+    st.write("### 키워드로 찾아보세요.")
     query = st.text_input("query", value="")
     max_results = st.text_input("max_results", value="100")
     day_num = st.text_input("day_num", value="0")
+    st.text(keyWordList)
+    if query in keyWordList:
+        path_csv = f'./data/{query}/*.csv'
+        csv_list = glob(path_csv)
+        all_df = pd.DataFrame()
+        for csv in csv_list:
+            tmp_df = pd.read_csv(csv)
+            all_df = pd.concat([all_df, tmp_df])
     if st.button("Confirm"):
         df = twitterAPI(query, int(max_results), int(day_num))
         st.dataframe(df, use_container_width=True)
-
+        all_df = pd.concat([all_df, df])
+        all_df.drop_duplicates(subset=['id', 'text', 'created_at'], keep='first')
+        st.dataframe(all_df, use_container_width=True)
 
 elif name == "Old Keyword":
 
-    path_csv = './data/*.csv'
-    csv_list = glob(path_csv)
+    
     st.title("Dataframe list")
     selected_option = st.radio('Select an option', csv_list)
     if selected_option:
